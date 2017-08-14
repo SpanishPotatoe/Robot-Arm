7079c0a837b8a35c1798753598e9343d67ac9915
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-14 13:13:56 -0400

    added button and joystick config

--- ArmCode.js
+++ ArmCode.js
@@ -83,3 +83,57 @@ board.on("ready", function() {
   // });
 
 });
+
+
+//Button setup
+board.on("ready", function() {
+
+  // Create a new `button` hardware instance.
+  // This example allows the button module to
+  // create a completely default instance
+  button = new five.Button(2);
+
+  // Inject the `button` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
+  board.repl.inject({
+    button: button
+  });
+
+  // Button Event API
+
+  // "down" the button is pressed
+  button.on("down", function() {
+    console.log("down");
+  });
+
+  // "hold" the button is pressed for specified time.
+  //        defaults to 500ms (1/2 second)
+  //        set
+  button.on("hold", function() {
+    console.log("hold");
+  });
+
+  // "up" the button is released
+  button.on("up", function() {
+    console.log("up");
+  });
+});
+
+
+//Joystick setup
+board.on("ready", function() {
+
+  // Create a new `joystick` hardware instance.
+  var joystick = new five.Joystick({
+    //   [ x, y ]
+    pins: ["A0", "A1"]
+  });
+
+  joystick.on("change", function() {
+    console.log("Joystick");
+    console.log("  x : ", this.x);
+    console.log("  y : ", this.y);
+    console.log("--------------------------------------");
+  });
+});
