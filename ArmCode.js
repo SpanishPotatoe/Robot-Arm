a90036b30d8fc92e452c76bdc4541e9a495052b3
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-22 18:28:49 -0400

    reset

--- ArmCode.js
+++ ArmCode.js
@@ -43,11 +43,6 @@ board.on("ready", function() {
         servo.step(2);
       }
 
-      if (this.y <= -0.5){
-        motor.start();
-      }
-
-
   });
 
 
@@ -110,42 +105,4 @@ board.on("ready", function() {
 
 
 
-});
-
-
-board.on("ready", function() {
-  // Create a new `motor` hardware instance.
-  motor = new five.Motor({
-    pin: 5
-  });
-
-  // Inject the `motor` hardware into
-  // the Repl instance's context;
-  // allows direct command line access
-  board.repl.inject({
-    motor: motor
-  });
-
-  // Motor Event API
-
-  // "start" events fire when the motor is started.
-  motor.on("start", function() {
-    console.log("start", Date.now());
-  });
-
-  // "stop" events fire when the motor is stopped.
-  motor.on("stop", function() {
-    console.log("stop", Date.now());
-  });
-
-  // Motor API
-
-  // start([speed)
-  // Start the motor. `isOn` property set to |true|
-  // Takes an optional parameter `speed` [0-255]
-  // to define the motor speed if a PWM Pin is
-  // used to connect the motor.
-
-  // stop()
-  // Stop the motor. `isOn` property set to |false|
 });
