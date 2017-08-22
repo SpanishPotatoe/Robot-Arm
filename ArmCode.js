3307ab0a94cfdfeb9eb7bc187c013cf8f6bcc36c
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-22 16:54:46 -0400

    test 1

--- ArmCode.js
+++ ArmCode.js
@@ -77,6 +77,31 @@ board.on("ready", function() {
   //
   // eg. array.step( -20 );
 
-servo.sweep();
+  function ServoMove() {
 
+    if (this.x > 0){
+      servo.sweep()
+    } else {
+      servo.to(90)
+    }
+
+  };
+
+});
+
+
+board.on("ready", function() {
+
+  // Create a new `joystick` hardware instance.
+  var joystick = new five.Joystick({
+    //   [ x, y ]
+    pins: ["A0", "A1"]
+  });
+
+  joystick.on("change", function() {
+    console.log("Joystick");
+    console.log("  x : ", this.x);
+    console.log("  y : ", this.y);
+    console.log("--------------------------------------");
+  });
 });
