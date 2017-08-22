cf5c1d646bb5cc4a9ee6c86762389fbd84b5f8b6
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-22 14:42:49 -0400

    added joystick

--- ArmCode.js
+++ ArmCode.js
@@ -78,3 +78,19 @@ board.on("ready", function() {
   // eg. array.step( -20 );
 
 });
+
+board.on("ready", function() {
+
+  // Create a new `joystick` hardware instance.
+  var joystick = new five.Joystick({
+    //   [ x, y ]
+    pins: ["A0", "A1"]
+  });
+
+  joystick.on("change", function() {
+    console.log("Joystick");
+    console.log("  x : ", this.x);
+    console.log("  y : ", this.y);
+    console.log("--------------------------------------");
+  });
+});
