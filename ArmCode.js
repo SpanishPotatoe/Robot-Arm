3d5aeafd4be53b8b01c978496a312602ba510e62
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-22 14:24:07 -0400

    testing servo instead, predicting motor problem from it being 3v on a 5v

--- ArmCode.js
+++ ArmCode.js
@@ -17,45 +17,65 @@
 // 2 Motor, 1 servo's, 1 joystick
 
 
-var five = require("johnny-five"),
-  board, motor, led;
-
-board = new five.Board();
+var five = require("johnny-five");
+var board = new five.Board();
 
 board.on("ready", function() {
-  // Create a new `motor` hardware instance.
-  motor = new five.Motor({
-    pin: 5
+  var servo = new five.Servo(10);
+
+  // Servo alternate constructor with options
+  /*
+  var servo = new five.Servo({
+    id: "MyServo",     // User defined id
+    pin: 10,           // Which pin is it attached to?
+    type: "standard",  // Default: "standard". Use "continuous" for continuous rotation servos
+    range: [0,180],    // Default: 0-180
+    fps: 100,          // Used to calculate rate of movement between positions
+    invert: false,     // Invert all specified positions
+    startAt: 90,       // Immediately move to a degree
+    center: true,      // overrides startAt if true and moves the servo to the center of the range
+  });
+  */
+
+  // Add servo to REPL (optional)
+  this.repl.inject({
+    servo: servo
   });
 
-  // Inject the `motor` hardware into
-  // the Repl instance's context;
-  // allows direct command line access
-  board.repl.inject({
-    motor: motor
-  });
 
-  // Motor Event API
+  // Servo API
 
-  // "start" events fire when the motor is started.
-  motor.on("start", function() {
-    console.log("start", Date.now());
-  });
+  // min()
+  //
+  // set the servo to the minimum degrees
+  // defaults to 0
+  //
+  // eg. servo.min();
 
-  // "stop" events fire when the motor is stopped.
-  motor.on("stop", function() {
-    console.log("stop", Date.now());
-  });
+  // max()
+  //
+  // set the servo to the maximum degrees
+  // defaults to 180
+  //
+  // eg. servo.max();
 
-  // Motor API
+  // center()
+  //
+  // centers the servo to 90°
+  //
+  // servo.center();
 
- start(40)
-  // Start the motor. `isOn` property set to |true|
-  // Takes an optional parameter `speed` [0-255]
-  // to define the motor speed if a PWM Pin is
-  // used to connect the motor.
-  motor.start();
+  // to( deg )
+  //
+  // Moves the servo to position by degrees
+  //
+  // servo.to( 90 );
 
-  // stop()
-  // Stop the motor. `isOn` property set to |false|
+  // step( deg )
+  //
+  // step all servos by deg
+  //
+  // eg. array.step( -20 );
+
+  servo.sweep();
 });
