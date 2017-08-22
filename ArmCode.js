b918db375ed832486752453e4bfa514400be204f
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-22 14:30:51 -0400

    test 4

--- ArmCode.js
+++ ArmCode.js
@@ -24,18 +24,18 @@ board.on("ready", function() {
   var servo = new five.Servo(10);
 
   // Servo alternate constructor with options
-  /*
+
   var servo = new five.Servo({
     id: "MyServo",     // User defined id
     pin: 10,           // Which pin is it attached to?
     type: "standard",  // Default: "standard". Use "continuous" for continuous rotation servos
-    range: [0,180],    // Default: 0-180
+    range: [20,160],    // Default: 0-180
     fps: 100,          // Used to calculate rate of movement between positions
     invert: false,     // Invert all specified positions
     startAt: 90,       // Immediately move to a degree
     center: true,      // overrides startAt if true and moves the servo to the center of the range
   });
-  */
+
 
   // Add servo to REPL (optional)
   this.repl.inject({
@@ -50,14 +50,14 @@ board.on("ready", function() {
   // set the servo to the minimum degrees
   // defaults to 0
   //
-   servo.min(80);
+  // eg. servo.min();
 
    //max()
   //
   // set the servo to the maximum degrees
   // defaults to 180
   //
-   servo.max(100);
+  // eg. servo.max();
 
   // center()
   //
