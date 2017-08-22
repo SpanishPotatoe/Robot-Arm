47cfb1c7308d399796c346eed3801cf0aaec9dd9
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-22 15:20:40 -0400

    testing different button config

--- ArmCode.js
+++ ArmCode.js
@@ -79,41 +79,25 @@ board.on("ready", function() {
 
 });
 
+
 var five = require("johnny-five"),
-  board, button;
+  button, led;
 
-board = new five.Board();
+five.Board().on("ready", function() {
 
-board.on("ready", function() {
-
-  // Create a new `button` hardware instance.
-  // This example allows the button module to
-  // create a completely default instance
-  button = new five.Button(2);
-
-  // Inject the `button` hardware into
-  // the Repl instance's context;
-  // allows direct command line access
-  board.repl.inject({
-    button: button
+  button = new five.Button({
+    pin: 2,
+    isPullup: true
   });
 
-  // Button Event API
+  led = new five.Led(13);
 
-  // "down" the button is pressed
-  button.on("down", function() {
-    console.log("down");
+  button.on("down", function(value) {
+    led.on();
   });
 
-  // "hold" the button is pressed for specified time.
-  //        defaults to 500ms (1/2 second)
-  //        set
-  button.on("hold", function() {
-    console.log("hold");
-  });
-
-  // "up" the button is released
   button.on("up", function() {
-    console.log("up");
+    led.off();
   });
+
 });
