18204cc686c0eee25e6d824029e50108a1ed62e5
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-22 14:06:53 -0400

    test 10

--- ArmCode.js
+++ ArmCode.js
@@ -54,7 +54,7 @@ board.on("ready", function() {
   // Takes an optional parameter `speed` [0-255]
   // to define the motor speed if a PWM Pin is
   // used to connect the motor.
-  motor.start(25);
+  motor.start(28);
 
   // stop()
   // Stop the motor. `isOn` property set to |false|
