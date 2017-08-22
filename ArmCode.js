f8ec4cacb2fca976c617ad66468eff888c6140e2
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-22 17:55:29 -0400

    test 1

--- ArmCode.js
+++ ArmCode.js
@@ -36,7 +36,7 @@ board.on("ready", function() {
     console.log("--------------------------------------");
 
       if(this.x >= 0.5){
-        servo.to(55);
+        servo.step(-55);
       }
 
       if(this.x <= -0.5){
