d61e06ea74a583f168aeb64efd147a28b39d2fd2
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-22 12:43:40 -0400

    change to top

--- ArmCode.js
+++ ArmCode.js
@@ -14,10 +14,27 @@
 
 // Coding for ArmBot: Javascript
 
-// 1 Motor, 3 servo's, 1 button, 1 joystick
+// 2 Motor, 1 servo's, 1 joystick
 
 
 // Board setup
 
 var five = require("johnny-five");
 var board = new five.Board();
+
+// Joystick function
+board.on("ready", function() {
+
+  // Create a new `joystick` hardware instance.
+  var joystick = new five.Joystick({
+    //   [ x, y ]
+    pins: ["A0", "A1"]
+  });
+
+  joystick.on("change", function() {
+    console.log("Joystick");
+    console.log("  x : ", this.x);
+    console.log("  y : ", this.y);
+    console.log("--------------------------------------");
+  });
+});
