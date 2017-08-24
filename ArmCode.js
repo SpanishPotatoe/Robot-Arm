b746da5a7e5b4af4dd9f662f91458397a5a0dce3
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-24 13:03:09 -0400

    test 1

--- ArmCode.js
+++ ArmCode.js
@@ -35,23 +35,17 @@ board.on("ready", function() {
     console.log("  y : ", this.y);
     console.log("--------------------------------------");
 
-
-    var ServoArray = [ turnServo() ];
-
-    function turnServo(){
-
-      if (this.x >= 0.5){
-      servo.step(-2);
-      }
-
-      if (this.x <= -0.5){
-      servo.step(2);
-      }
-    }
-
-    ServoArray[0]
-
   });
+
+  if (this.x >= 0.5){
+  servo.step(-2);
+  }
+
+  if (this.x <= -0.5){
+  servo.step(2);
+  }
+  }
+
 
 
   // Create a new 'servo' hardware instance
