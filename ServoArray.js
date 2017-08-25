f644caa860c5f2332221f102b5341c6e991c3759
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-25 14:20:35 -0400

    test

--- ServoArray.js
+++ ServoArray.js
@@ -5,7 +5,7 @@
 
 //var ServoArray = [  "Message Received", 10, LeftRight() ]
 
-
+var ServoArray = {
       if (this.x >= 0.5){
       servo.to(155)
     }
@@ -17,7 +17,7 @@
     if (this.x <= -0.5){
       servo.to(55)
     }
-
+}
 
 
 module.exports = ServoArray;
