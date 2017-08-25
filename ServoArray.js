5d75f4bba1807c9bef405221029978040bb12336
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-25 12:27:33 -0400

    fix

--- ServoArray.js
+++ ServoArray.js
@@ -10,7 +10,7 @@ var ServoArray = [  "Message Received", 10, LeftRight() ]
   if (this.x >= 0.5){
     console.log("X IS MAXED");
   }
-  
+
 };
 
-module.exports = ServoArray;
+module.exports = ServoArray, LeftRight;
