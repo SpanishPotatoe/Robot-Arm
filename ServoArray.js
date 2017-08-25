affa52e6c91136b448ae5e3bbddcbb02d72ca0c7
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-25 12:24:21 -0400

    test 1

--- ServoArray.js
+++ ServoArray.js
@@ -3,9 +3,14 @@
 // NOT JOHNNY-FIVE, ADDED FOR PROGRAMMING PURPOSE
 
 
-var ServoArray = [  "Message Received", 10 ]
+var ServoArray = [  "Message Received", 10, LeftRight() ]
 
+  function LeftRight(){
 
-
+  if (this.x >= 0.5){
+    console.log("X IS MAXED");
+  }
+  
+};
 
 module.exports = ServoArray;
