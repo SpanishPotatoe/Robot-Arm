273e5c3fc2584dbbf556a5e879070a5f7d995fd9
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-25 12:50:35 -0400

    reset array

--- ServoArray.js
+++ ServoArray.js
@@ -3,14 +3,10 @@
 // NOT JOHNNY-FIVE, ADDED FOR PROGRAMMING PURPOSE
 
 
+var ServoArray = [  "Message Received", 10 ]
 
-var ServoArray =
 
-if (this.x >= 0.5){
-  servo.to(155);
-}
 
-};
 
 
 module.exports = ServoArray;
