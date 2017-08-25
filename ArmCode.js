786155881eea6c7c71e05cf158453309370d0b26
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-25 12:00:27 -0400

    test 1

--- ArmCode.js
+++ ArmCode.js
@@ -35,10 +35,10 @@ board.on("ready", function() {
     console.log("  y : ", this.y);
     console.log("--------------------------------------");
 
-
+    var ServoArray = require ('./ServoArray')
 
       if (this.y >= 0.5){
-        return ServoArray
+        ServoArray
       }
 
 
