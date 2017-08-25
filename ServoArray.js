34dd6603aeb58d81d33d4c2205ca8a0e2625b09d
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-25 14:04:55 -0400

    testing for argument config

--- ServoArray.js
+++ ServoArray.js
@@ -3,22 +3,24 @@
 // NOT JOHNNY-FIVE, ADDED FOR PROGRAMMING PURPOSE
 
 
-var ServoArray = [  "Message Received", 10 ]
+var ServoArray = [  "Message Received", 10, LeftRight() ]
 
 
 
-if (this.x >= 0.5){
-  servo.to(155)
+  function LeftRight(){
+
+      if (this.x >= 0.5){
+      servo.to(155)
+    }
+
+    if (this.x <= 0.5 && this.x >= -0.5){
+      servo.to(105)
+    }
+
+    if (this.x <= -0.5){
+      servo.to(55)
+    }
 }
-
-if (this.x <= 0.5 && this.x >= -0.5){
-  servo.to(105)
-}
-
-if (this.x <= -0.5){
-  servo.to(55)
-}
-
 
 
 module.exports = ServoArray;
