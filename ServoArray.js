cc1cab929a216dd8921a8b7a4812eeaca647a337
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-25 12:47:55 -0400

    test 1

--- ServoArray.js
+++ ServoArray.js
@@ -3,8 +3,13 @@
 // NOT JOHNNY-FIVE, ADDED FOR PROGRAMMING PURPOSE
 
 
-var ServoArray = [  "Message Received", 10 ]
+  function ServoArray() {
 
+if (this.x >= 0.5){
+  servo.to(155);
+}
+
+};
 
 
 module.exports = ServoArray;
