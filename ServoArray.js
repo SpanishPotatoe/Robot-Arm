cdc11c838e089a81943eebc3e4b997c494012f1e
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-25 11:30:55 -0400

    Setup test 1

--- ServoArray.js
+++ ServoArray.js
@@ -1,1 +1,28 @@
 
+
+// NOT JOHNNY-FIVE, ADDED FOR PROGRAMMING PURPOSE
+
+
+ServoArray [ LeftRight() ]
+
+
+
+
+  function LeftRight(){
+
+    if (this.x >= 0.5){
+      servo.to(155);
+      }
+
+    if (this.x <= 0.5 && this.x >= -0.5){
+      servo.to(115);
+      }
+
+    if (this.x <= -0.5){
+      servo.to(75);
+      }
+
+    }
+
+
+module.export = ServoArray;
