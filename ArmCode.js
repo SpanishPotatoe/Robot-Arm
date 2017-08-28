59c5c61d790355f6a672e9f1aec2147c93e64e5c
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-28 15:46:44 -0400

    test

--- ArmCode.js
+++ ArmCode.js
@@ -17,7 +17,9 @@
 // 2 Motor, 1 servo's, 1 joystick
 
 
-var five = require("johnny-five");
+var five = require("johnny-five"),
+  board, lcd;
+
 var board = new five.Board();
 
 
@@ -48,13 +50,13 @@ board.on("ready", function() {
   });
 
   // Create a new 'servo' hardware instance
-  var servo = new five.Servo(10);
+  var servo = new five.Servo(5);
 
   // Servo alternate constructor with options
 
   var servo = new five.Servo({
     id: "MyServo",     // User defined id
-    pin: 10,           // Which pin is it attached to?
+    pin: 5,           // Which pin is it attached to?
     type: "standard",  // Default: "standard". Use "continuous" for continuous rotation servos
     range: [55,155],    // Default: 0-180
     fps: 100,          // Used to calculate rate of movement between positions
@@ -108,5 +110,43 @@ board.on("ready", function() {
 
   // "blink" the led in 500ms on-off phase periods
   led.blink(500);
+
+
+  lcd = new five.LCD({
+      // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
+      // Arduino pin # 7    8   9   10  11  12
+      pins: [7, 8, 9, 10, 11, 12],
+      backlight: 6,
+      rows: 2,
+      cols: 20
+
+
+      // Options:
+      // bitMode: 4 or 8, defaults to 4
+      // lines: number of lines, defaults to 2
+      // dots: matrix dimensions, defaults to "5x8"
+    });
+
+    // Tell the LCD you will use these characters:
+    lcd.useChar("check");
+    lcd.useChar("heart");
+    lcd.useChar("duck");
+
+    // Line 1: Hi rmurphey & hgstrp!
+    lcd.clear().print("rmurphey, hgstrp");
+    lcd.cursor(1, 0);
+
+    // Line 2: I <3 johnny-five
+    // lcd.print("I").write(7).print(" johnny-five");
+    // can now be written as:
+    lcd.print("I :heart: johnny-five");
+
+    this.wait(3000, function() {
+      lcd.clear().cursor(0, 0).print("I :check::heart: 2 :duck: :)");
+    });
+
+    this.repl.inject({
+      lcd: lcd
+    });
 
 });
