99e612144aa5053212e0b065fce2e8580e99ffe6
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-29 10:59:50 -0400

    reset

--- ArmCode.js
+++ ArmCode.js
@@ -45,10 +45,11 @@ board.on("ready", function() {
       lcd.clear().print("Servo Mode : ");
       lcd.cursor(1, 0);
       lcd.print("Left Right");
-      Object.create(ServoArray);
+
+
     }
 
-    if ( ServoArray = true ){
+    if (  = true ){
 
       if (this.x >= 0.5){
       servo.to(70)
