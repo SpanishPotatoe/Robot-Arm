ee4a0e02a32754b76eadfaa86dbf60bf795fa888
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-29 18:53:28 -0400

    attempt fix

--- ArmCode.js
+++ ArmCode.js
@@ -117,7 +117,7 @@ console.log(joystickPosition);
     fps: 100,          // Used to calculate rate of movement between positions
     invert: false,     // Invert all specified positions
     startAt: 95,       // Immediately move to a degree
-    center: true,      // overrides startAt if true and moves the servo to the center of the range
+    center: false,      // overrides startAt if true and moves the servo to the center of the range
   });
   // Add servo to REPL (optional)
   this.repl.inject({
