e4d01d08e0d3226eb3f3a42cf93ee1f2975cb58f
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-29 12:02:17 -0400

    test 12

--- ArmCode.js
+++ ArmCode.js
@@ -41,14 +41,14 @@ board.on("ready", function() {
 
     var ServoArray = require ('./ServoArray');
 
-    var leftright
+    var global.leftright
 
     if (this.y >= 0.5){
       lcd.clear().print("Servo Mode : ");
       lcd.cursor(1, 0);
       lcd.print("Left Right");
 
-      var leftright = true
+      var global.leftright = true
 
     }
 
@@ -73,7 +73,7 @@ board.on("ready", function() {
     lcd.cursor(1, 0);
     lcd.print("Turn Degree");
 
-     var leftright = false
+     var global.leftright = false
   }
 
 
