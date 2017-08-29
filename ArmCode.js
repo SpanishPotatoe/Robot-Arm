4bcfbbca358918ff59a5a9b1f0f84fa34ed1ae27
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-29 17:04:40 -0400

    changes to code

--- ArmCode.js
+++ ArmCode.js
@@ -22,48 +22,42 @@ board.on("ready", function() {
     //   [ x, y ]
     pins: ["A0", "A1"]
   });
-var LeftRight = ['1'];
+var joystickPosition = "center";
 joystick.on("change", function() {
-    console.log("Joystick");
-    console.log("  x : ", this.x);
-    console.log("  y : ", this.y);
-    console.log("--------------------------------------");
-  function  Servostring1(){
-      LeftRight.shift();
-  }
+  console.log("Joystick");
+  console.log("  x : ", this.x);
+  console.log("  y : ", this.y);
+  console.log("--------------------------------------");
   if (this.y >= 0.75){
     lcd.clear().print("Servo Mode : ");
     lcd.cursor(1, 0);
     lcd.print("Left Right");
-    Servostring1();
+    joystickPosition = "up"
   }
-  if ( LeftRight[0] == null ){
-        if (this.x >= 0.5){
-          servo.to(70);
-        }
-        if (this.x < 0.5 && this.x > -0.5){
-          servo.to(105);
-        }
-        if (this.x <= -0.5){
-          servo.to(155);
-        }
-  }
-  function  Servostring2(){
-    LeftRight.unshift('1');
+  if ( joystickPosition == "up" ){
+    if (this.x >= 0.5){
+      servo.to(70);
+    }
+    if (this.x < 0.5 && this.x > -0.5){
+      servo.to(105);
+    }
+    if (this.x <= -0.5){
+      servo.to(155);
+    }
   }
   if (this.y <= -0.75){
     lcd.clear().print("Servo Mode : ");
     lcd.cursor(1, 0);
     lcd.print("Turn Degree");
-    Servostring2();
+    joystickPosition = "down"
   }
-  if ( LeftRight[0] == '1' ){
-          if (this.x >= 0.5){
-            servo.step(-2);
-          }
-          if (this.x <= -0.5){
-            servo.step(+2);
-          }
+  if ( joystickPosition = "down" ){
+    if (this.x >= 0.5){
+      servo.step(-2);
+    }
+    if (this.x <= -0.5){
+      servo.step(+2);
+    }
   }
 console.log(LeftRight);
 });
