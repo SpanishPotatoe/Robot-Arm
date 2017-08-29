44abcb9a3e9513d16982a9a0bfd5010d99723116
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-29 14:06:29 -0400

    test 27

--- ArmCode.js
+++ ArmCode.js
@@ -40,14 +40,16 @@ board.on("ready", function() {
 
     var ServoArray = require ('./ServoArray');
 
-    var leftRight
+    function SetToOne(){
+      var leftRight = 1
+    }
 
 
     if (this.y >= 0.75){
       lcd.clear().print("Servo Mode : ");
       lcd.cursor(1, 0);
       lcd.print("Left Right");
-      var leftRight = 1
+      SetToOne();
     }
 
 
