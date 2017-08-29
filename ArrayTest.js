336e16a41bbacd8aca7093bd92e421bca11a4335
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-29 14:24:26 -0400

    t

--- ArrayTest.js
+++ ArrayTest.js
@@ -12,14 +12,14 @@ console.log( "I WORK!");
 console.log(ServoArray[0]);
 
 */
-  var value1 = console.log();
+  var value1 = console.log(1);
 
 var cheese = .75
 
-  function JoystickToggle(value){
+  function JoystickToggle(){
     if (cheese >= .75 ){
       Object.create(value1);
     }
 
-  JoystickToggle(1);
+  JoystickToggle();
   }
