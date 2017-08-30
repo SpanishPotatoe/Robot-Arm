8f41aa106eb59aea98acaaea309b18305c4aaa4a
Daniel Cardona <daniel.cardona1998@gmail.com>
2017-08-30 11:03:10 -0400

    Update myCode.js

--- myCode.js
+++ myCode.js
@@ -1,46 +1,45 @@
-var servoMode = null
-var joystickPosition = "center";
-joystick.on("change", function() {
-  //Determine position of joystick
-  if ((Math.round(this.y * 100) / 100) >  0.9  )         { joystickPosition = "down"  }
-  if ((Math.round(this.x * 100) / 100) >= 0.5  )         { joystickPosition = "lt"    }
-  if (Math.round(this.y) == 0 && Math.round(this.x) == 0){ joystickPosition = "center"}
-  if ((Math.round(this.x * 100) / 100) <= -0.5 )         { joystickPosition = "rt"    }
-  if (Math.round(this.y) == -1)                          { joystickPosition = "up"    }
-  console.log("Joystick", servoMode);
-  console.log("  x : ", this.x);
-  console.log("  y : ", this.y);
-  console.log("--------------------------------------");
-  //Handle joystick up
-  if (joystickPosition == "up"){
-    //enter servo mode : full swing back
-    lcd.clear().print("Servo Mode : ");
-    lcd.cursor(1, 0);
-    servoMode = 'FSB'
-    lcd.print("Full Swing Back");
-  }
-  //Handle joystick down
-  if (joystickPosition == "down"){
-    //enter servo mode : Incremental hold
-    lcd.clear().print("Servo Mode : ");
-    lcd.cursor(1, 0);
-    servoMode = 'IH'
-    lcd.print("Incremental hold");
-  }
-  //Handle joystick center
-  if (joystickPosition == "center"){
-    //determine Servo  mode
-    if (servoMode == 'FSB'){  servo.to(105);  }
-  }
-  //Handle joystick rt
-  if (joystickPosition == "rt"){
-    //determine Servo  mode
-    if (servoMode == 'FSB'){  servo.to(155);  }
-    if (servoMode == 'IH') {  servo.step(+2); }
-  }
-  //Handle joystick lt
-  if (joystickPosition == "lt"){
-    //determine Servo  mode
-    if (servoMode == 'FSB'){   servo.to(70);  }
-    if (servoMode == 'IH'){   servo.step(-2)  }
-  }
+var LeftRight = ['1'];
+ joystick.on("change", function() {
+     console.log("Joystick");
+     console.log("  x : ", this.x);
+     console.log("  y : ", this.y);
+     console.log("--------------------------------------");
+   function  Servostring1(){
+       LeftRight.shift();
+   }
+   if (this.y >= 0.75){
+     lcd.clear().print("Servo Mode : ");
+     lcd.cursor(1, 0);
+     lcd.print("Left Right");
+     Servostring1();
+   }
+   if ( LeftRight[0] == null ){
+         if (this.x >= 0.5){
+           servo.to(70);
+         }
+         if (this.x < 0.5 && this.x > -0.5){
+           servo.to(105);
+         }
+         if (this.x <= -0.5){
+           servo.to(155);
+         }
+   }
+   function  Servostring2(){
+     LeftRight.unshift('1');
+   }
+   if (this.y <= -0.75){
+     lcd.clear().print("Servo Mode : ");
+     lcd.cursor(1, 0);
+     lcd.print("Turn Degree");
+     Servostring2();
+   }
+   if ( LeftRight[0] == '1' ){
+           if (this.x >= 0.5){
+             servo.step(-2);
+           }
+           if (this.x <= -0.5){
+             servo.step(+2);
+           }
+   }
+ console.log(LeftRight);
+ });
